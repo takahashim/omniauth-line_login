b3cf2b9e19425df4a29c2eca3a123a71fdb82825
takahashim <takahashimm@gmail.com>
2022-04-22 19:39:11 +0900

    bundle new

---
+++ lib/omniauth/line_login.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "line_login/version"
+
+module Omniauth
+  module LineLogin
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/line_login_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::LineLogin do
+  it "has a version number" do
+    expect(Omniauth::LineLogin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
